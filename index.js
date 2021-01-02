bfb454e406f1a0ed2eb678c83d9c2a958f7b2a03
Yashwant Rautela <yashwant.rautela@intelligaia.com>
2021-01-02 21:43:11 +0530

    group chat working

--- index.js
+++ index.js
@@ -1,24 +1,16 @@
 var koa = require('koa');
 var koaRouter = require('koa-router');
-var IO = require('koa-socket-2');
 var serve = require('koa-static');
 require('dotenv').config();
 var port = process.env.PORT || 4000;
 var app = new koa();
 var router = new koaRouter();
-var io = new IO();
+var socket = require('./app/socket');
+socket(app);
 router.get('/api', (ctx) => {
     ctx.body = 'hello world';
 });
 app.use(serve('./public'))
-io.attach(app);
-io.on('connection', (socket) => {
-    console.log('user connected', socket.id);
-    socket.on('chat message', (msg) => {
-        console.log('message: ' + msg);
-        socket.emit('chat message', msg);
-    });
-});
 app.use(router.routes())
     .use(router.allowedMethods());
 app.listen((port), () => {
